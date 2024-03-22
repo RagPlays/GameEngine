a91d390b8fe9cf7d519ea4b9e622a9da80b9cf9a
Maurice Vandenheede <maurice.vandenheede@gmail.com>
2024-03-22 09:49:37 +0100

    Starting with the observer

--- Engine/Observer.h
+++ Engine/Observer.h
@@ -1,13 +1,27 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
-//class Observer
-//{
-//public:
-//
-//	Observer() = default;
-//	virtual ~Observer() {}
-//	virtual void onNotify(const Entity& entity, Event event) = 0;
-//};
+#include "GameEvents.h"
+
+class PlayerComponent;
+
+class Observer
+{
+public:
+
+	virtual ~Observer() = default;
+
+	Observer(const Observer& other) = delete;
+	Observer(Observer&& other) noexcept = delete;
+	Observer& operator=(const Observer& other) = delete;
+	Observer& operator=(Observer&& other) noexcept = delete;
+
+	virtual void OnNotify(PlayerComponent* entity, GameEvent event) = 0;
+
+protected:
+
+	Observer() = default;
+
+};
 
 #endif // !OBSERVER_H
